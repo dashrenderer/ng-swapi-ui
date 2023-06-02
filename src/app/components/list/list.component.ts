2c86dbc3a4931eb009607b142d78bab5a0dec1c5
Matt Hiebert <hiem86@gmail.com>
2023-06-02 01:28:12 -0500

    Add routerlink to thumbnail

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ListComponent<T> {
   @Input() entityType!: string | null;
--- src/app/components/thumbnail/thumbnail.component.ts
+++ src/app/components/thumbnail/thumbnail.component.ts
@@ -1,13 +1,34 @@
-import { Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-thumbnail',
   templateUrl: './thumbnail.component.html',
   styleUrls: ['./thumbnail.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ThumbnailComponent {
+export class ThumbnailComponent implements OnChanges {
   @Input() type!: string | null;
 
   // todo: improve typing for this field
   @Input() entity!: any;
+
+  entityId: string | undefined;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['entity']) {
+      this.entityId = this.extractEntityId(this.entity['url']);
+    }
+  }
+
+  private extractEntityId(url: string): string {
+    const parts = url.split('/').filter((part) => part.length > 0);
+
+    return parts[parts.length - 1];
+  }
 }
