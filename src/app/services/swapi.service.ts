86cf94dabdfe45f3f3ca574c904819bf1b3b56dc
dashrenderer <hiem86@gmail.com>
2023-06-02 10:57:38 -0500

    Memoize individual entities

--- src/app/services/swapi.service.ts
+++ src/app/services/swapi.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 
 import { ApiResult } from '../models';
 
@@ -10,6 +10,8 @@ const BASE_URL = 'https://swapi.dev/api/';
   providedIn: 'root',
 })
 export class SwapiService {
+  private cachedEntities = new Map<string, any>();
+
   constructor(private http: HttpClient) {}
 
   getEntitiesByType<T = any>(
@@ -21,16 +23,25 @@ export class SwapiService {
       url += '/?search=' + search;
     }
 
-    return this.http.get<ApiResult<T>>(url);
+    return this.http.get<ApiResult<T>>(url).pipe(tap(response => {
+      response.results.forEach(result => {
+        this.cachedEntities.set((result as any).url, result);
+      })
+    }));
   }
 
   getEntityById<T = any>(type: string, id: string): Observable<T> {
     const url = `${BASE_URL}${type}/${id}`;
 
-    return this.http.get<T>(url);
+    return this.getEntityByUrl<T>(url);
   }
 
   getEntityByUrl<T = any>(url: string): Observable<T> {
-    return this.http.get<T>(url);
+    // todo: move this to an HTTP interceptor
+    if (this.cachedEntities.has(url)) {
+      return of(this.cachedEntities.get(url));
+    }
+
+    return this.http.get<T>(url).pipe(tap(result => this.cachedEntities.set(url, result)));
   }
 }
